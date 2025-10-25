8c94e54e12ec03addbdf33b03dccf9cfc0c718e6
Uppala Harshavardhan <Uppala Harshavardhan@FFE-Uppala-Harshavardhan>
2025-10-25 12:30:35 +0530

    commited

--- Pet_Store_project/src/test/java/com/petStore/store/StoreOperations.java
+++ Pet_Store_project/src/test/java/com/petStore/store/StoreOperations.java
@@ -5,8 +5,11 @@ import org.testng.annotations.Test;
 import com.PetStore.BaseAPIClass.BaseAPIClass;
 
 import iEndPoints.StoreEndPoints;
+import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.*;
+
+import java.util.ArrayList;
 
 public class StoreOperations extends BaseAPIClass{
 	
@@ -15,12 +18,19 @@ public class StoreOperations extends BaseAPIClass{
 	public void getInventory()
 	{
 		given()
-		.spec(requestSpecification)
-		.log().all()
-		.get(StoreEndPoints.GET_INVENTORY)
+		.spec(reqSpecObject)
+		.basePath(StoreEndPoints.GET_INVENTORY)
+		.get()
 		.then()
-		.spec(responseSpecification)
-		.assertThat().statusCode(200);
+		.assertThat().statusCode(200)
+		.spec(resSpecObject)
+		.log().all();
+		
+	}
+	
+	@Test
+	public void placeOrder()
+	{
 		
 	}
 	
