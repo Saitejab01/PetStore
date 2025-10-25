9b820e6d0c8941a52e8f27c2634479e330bb6d5d
Bobbala Venkata Nagi <Bobbala Venkata Nagi@FFE-Bobbala-Venkata-Nagireddy>
2025-10-25 12:06:49 +0530

    Merge branch 'master' into userSubBranch

--- Pet_Store_project/src/test/java/com/petStore/user/CreateUserTest.java
+++ Pet_Store_project/src/test/java/com/petStore/user/CreateUserTest.java
@@ -2,17 +2,24 @@ package com.petStore.user;
 
 import org.testng.annotations.Test;
 
+import com.PetStore.BaseAPIClass.BaseAPIClass;
+
 import POJO.util.CreateUserPojo;
-import io.restassured.RestAssured;
+import iEndPoints.UserEndPoints;
 
 import static io.restassured.RestAssured.*;
 
-public class CreateUserTest {
+public class CreateUserTest extends BaseAPIClass {
 	@Test
 	public void createUser() {
-		RestAssured.baseURI = "";
 		given()
-			.body(new CreateUserPojo());
+			.spec(reqSpecObject)
+			.body(new CreateUserPojo(2,"Venkat123","venkat","user","sample@gmail.com","sample@123","1234554321",0))
+		.when()
+			.post(UserEndPoints.CREATE_USER)
+		.then()
+			.spec(resSpecObject)
+			.log().all();
 			
 	}
 }
